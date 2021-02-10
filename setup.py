9967cd92d31882f90b9a3f15e80d427bac789188
Francis Vitt <fvitt@ucar.edu>
2021-02-10 13:47:03 -0700

    set up

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools import setuptools
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package",
+    author="Vitt",
+    author_email="fvitt@ucar.edu",
+    packages=["mysci"],
+    install_requires=[])
+
